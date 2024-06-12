b1094c8fbf8308545c19d387c4e2dd883b0445f6
Hani <139141380+Darkksoul@users.noreply.github.com>
2024-06-12 17:38:58 +0530

    Update exceptions.py

--- bot/helper/ext_utils/exceptions.py
+++ bot/helper/ext_utils/exceptions.py
@@ -1,14 +1,8 @@
 class DirectDownloadLinkException(Exception):
     pass
 
-
 class NotSupportedExtractionArchive(Exception):
     pass
-
-
-class RssShutdownException(Exception):
-    pass
-
 
 class TgLinkException(Exception):
     pass
